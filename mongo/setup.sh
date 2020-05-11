7ba550b8a11c99345ece1df7f5a3a9d531ac60be
wafu7969@qq.com <7969187>
2020-05-11 11:00:22 +0800

    添加alpine分支，使构建的镜像更小

--- mongo/setup.sh
+++ mongo/setup.sh
@@ -4,7 +4,7 @@ mongo <<EOF
 use admin;
 db.createUser({ 
     user: "admin", 
-    pwd: "123456",
+    pwd: "j8K9aPm1v4z",
     roles: [ { role: "root", db: "admin" } ], 
     mechanisms : ["SCRAM-SHA-1"]
 });
@@ -12,7 +12,7 @@ use robot;
 db.createUser(
     {
         user: "robot",
-        pwd: "kd0a-dlfzdl",
+        pwd: "k3d0adlf5z7dl",
         roles: [
 			{role:"read",db:"admin"},
             { role: "readWrite", db: "robot"}
@@ -23,17 +23,21 @@ db.createUser(
 db.createCollection("member");
 db.member.insert(
 	{
-		"user_name" : "admin",
-		"password" : "e10adc33e5f759abbe56e057f20f883e",
-		"company" : "公司",
-		"contact_number" : "13188888888",
-		"validity" : 1562774400,
-		"create_time" : 1562607938,
-		"update_time" : 1562607938,
-		"role" : 1,
-		"status" : 1,
-		"del" : 0,
-		"id" : "5d238142320fe81ec00056f2"
+        "user_name" : "admin",
+        "password" : "d4d5542ea754f5a121ca0d9f293c7115",
+        "company" : "company",
+        "contact_number" : "",
+        "validity" : 1632495600,
+        "create_time" : 1563010552,
+        "update_time" : 1563010552,
+        "role" : "1",
+        "status" : 1,
+        "del" : 0,
+        "id" : "5d29a5f805b794000f55c632",
+        "application" : "introduce,staticexplain,library,slides,trailers,newspaper,lawsuit,voicemail,dishonests,copyfile,autoexplain,guide,customexplain,custom",
+        "left_menu_role" : "1aic,3dLo,9Em3,12uM3,11dMk,7Jf0,8uMD,4ufD",
+        "__token__" : "819cfb153d844c9488044674d789694d",
+        "up_size" : 1151144069
 	}
 );
 
